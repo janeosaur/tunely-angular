639595cbfaa1a7cf35c483d5eff328c31354e1cc
janeosaur <janewie@gmail.com>
2017-04-27 11:49:08 -0700

    create delete songs

--- public/scripts/controllers/AlbumsShowController.js
+++ public/scripts/controllers/AlbumsShowController.js
@@ -17,4 +17,29 @@ function AlbumsShowController ($http, $routeParams) {
     console.log('There was an error getting the data', response);
   });
 
+  vm.createSong = function () {
+    $http({
+      method: 'POST',
+      url: '/api/albums/' +$routeParams.id + '/songs' , // /api/albums/:albumId/songs
+      data: vm.newSong
+    }).then(function successCallback(response) {
+      console.log('vm is ', vm.newSong);
+      console.log('response is ', response);
+      vm.album.songs.push(vm.newSong);
+    }, function errorCallback(response) {
+      console.log('There was an error posting the data', response);
+    });
+  }
+
+  vm.deleteSong = function (song) {
+    $http({
+      method: 'DELETE',
+      url: '/api/albums/' +$routeParams.id + '/songs/' + song._id
+    }).then(function successCallback(json) {
+      var index = vm.album.songs.indexOf(song);
+      vm.album.songs.splice(index,1)
+    }, function errorCallback(response) {
+      console.log('There was an error deleting the data', response);
+    });
+  }
 }
